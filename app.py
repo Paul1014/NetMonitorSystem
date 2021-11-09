a080e14200bce12d330f5c8ea3aac4bc561231a9
Paul1014 <happy0920157@gmail.com>
2021-11-09 20:43:34 +0800

    New device API, device status page

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import render_template,redirect,url_for,request
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 from flask_login import  UserMixin, LoginManager, login_required, current_user, login_user, logout_user
-from net_tool import Connect_SSH, cisco_snmp, linux_snmp
+from net_tool import Connect_SSH, cisco_snmp, linux_snmp, snmp_get_interfaces_traffic
+import json
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Netmonitor.db'
@@ -36,19 +38,39 @@ class Devices(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     IP = db.Column(db.String(80), nullable=False)
     DeviceTypes = db.Column(db.String(80), nullable=False)
+    snmp_string = db.Column(db.String(80), nullable=False)
     login_username = db.Column(db.String(80))
     login_password = db.Column(db.String(80))
     
     
-    def __init__(self, name, IP, DeviceTypes, login_username, login_password):
+    def __init__(self, name, IP, DeviceTypes, snmp_string, login_username, login_password):
         self.name = name
         self.IP = IP
         self.DeviceTypes = DeviceTypes
+        self.snmp_string = snmp_string
         self.login_username = login_username
         self.login_password = login_password
         
     def __repr__(self):
         return '<Devices %r>'% self.name
+        
+class Devices_SNMP(db.Model):
+    __tablename__ = "Devices_SNMP"
+    device_id = db.Column(db.Integer, primary_key=True)
+    hostname = db.Column(db.String(80))
+    uptime = db.Column(db.String(80))
+    memory_usage = db.Column(db.String(80))
+    traffic_flow = db.Column(db.String(80))
+    
+    
+    def __init__(self, device_id, hostname, uptime, memory_usage):
+        self.device_id = device_id
+        self.hostname = hostname
+        self.uptime = uptime
+        self.memory_usage = memory_usage
+        
+    def __repr__(self):
+        return '<Devices_SNMP %r>'% self.name
 
 class UserAuth(UserMixin):
     pass
@@ -115,6 +137,7 @@ def add():
             username = request.form['username']
             password = request.form['password']       
             device_type = request.form['devicestype']
+            snmp_string = request.form['snmp_string']
             testssh = request.form['testconnect']
 
             if testssh == "yes":
@@ -122,19 +145,20 @@ def add():
                     pass
                 else:
                     return render_template('add_form.html',status="The SSH session cannot established")
-
             if IP == "" or name == "":
                 return render_template('add_form.html',status="Some filed is not filled, plz check!")
+
             else:
                 db.create_all()
                 try:
-                    New_Device = Devices(name, IP, device_type, username, password)
+                    New_Device = Devices(name, IP, device_type, snmp_string, username, password)
                     db.session.add(New_Device)
                     db.session.commit()
                 except:
                     return render_template('add_form.html',status="Devices is existed")
 
                 return render_template('add_form.html',status="Add success")
+
     else:
         return redirect(url_for('login')) 
 
@@ -143,12 +167,10 @@ def add():
 def devices_status(id):
     if current_user.is_active:
         device = Devices.query.filter_by(device_id=id).first()
-        if device.DeviceTypes == "Cisco":
-            snmpinfo = cisco_snmp(device.IP,"paulsnmp")
-        else:
-            snmpinfo = linux_snmp(device.IP,"public")
 
-        return render_template('device_cisco.html', snmp_info=snmpinfo, deviceID=id)
+        interfaces = snmp_get_interfaces_traffic(device.IP,device.snmp_string)
+
+        return render_template('device_cisco.html', interfaces=interfaces ,deviceID=id)
     else:
         return redirect(url_for('login'))
 
@@ -162,6 +184,16 @@ def delete(id):
         return redirect(url_for('dashboard'))
     else:
         return redirect(url_for('login'))
+
+@app.route('/api/device/<id>')
+def device_api(id):
+    device = Devices.query.filter_by(device_id=id).first()
+    IP = device.IP
+    community = device.snmp_string
+    snmp_info = cisco_snmp(IP,community)
+    snmp_info["interfaces"] = snmp_get_interfaces_traffic(IP,community)
+    snmp_info = json.dumps(snmp_info)
+    return snmp_info
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
